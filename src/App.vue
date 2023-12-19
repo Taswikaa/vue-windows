2f0bb256e0966edc96bc9ab3efe13253095d309a
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-12-19 16:41:00 +0300

    настройка проекта

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <CreatingForm />
+  </div>
+</template>
+
+<script>
+import CreatingForm from './components/CreatingForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CreatingForm
+  }
+}
+</script>
+
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .app {
+    background-color: #252525;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 50px;
+    color: #fff;
+  }
+</style>
---
+++ src/components/CreatingForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>form</h2>
+</template>
+
+<script>
+export default {
+  name: 'CreatingForm',
+}
+</script>
+
+<style>
+
+</style>
